c86c335fdc089e90fe4a4c64d4b0102363e287ae
Oguzhan Yilmaz <ogzhnylmz5@gmail.com>
2023-04-14 12:17:38 +0300

    Add project files.

---
+++ RequestHW/Controllers/RequestController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RequestHW.Models;
+
+namespace RequestHW.Controllers
+{
+    public class RequestController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            List<JsonPlaceholderModel> olist = new List<JsonPlaceholderModel>();
+            using(var client = new HttpClient()) 
+            {
+                client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/photos");
+                var response = await client.GetAsync("photos");
+                if (response.IsSuccessStatusCode)
+                {
+                    var readTask = await response.Content.ReadAsStringAsync();
+                    olist = JsonConvert.DeserializeObject<List<JsonPlaceholderModel>>(readTask);
+                }
+            }
+            return View(olist);
+        }
+
+        //public async Task<IActionResult> Index()
+        //{
+        //    List<Ogrenci> olist = new List<Ogrenci>();
+        //    using (var client = new HttpClient())
+        //    {
+        //        client.BaseAddress = new Uri("https://localhost:7254/api/Ogranci/ListOgrenci/ListOgrenci");
+        //        var response = await client.GetAsync("ListOgrenci");
+        //        if (response.IsSuccessStatusCode)
+        //        {
+        //            var readTask = await response.Content.ReadAsStringAsync();
+
+        //            olist = JsonConvert.DeserializeObject<List<Ogrenci>>(readTask);
+
+        //        }
+        //    }
+        //    return View(olist);
+        //}
+    }
+}
---
+++ RequestHW/Models/JsonPlaceholderModel.cs
@@ -0,0 +1,16 @@
+﻿namespace RequestHW.Models
+{
+    public class JsonPlaceholderModel
+    {
+        public int albumId { get; set; }
+
+        public int id { get; set; }
+
+        public string title { get; set; }
+
+        public string url { get; set; }
+
+        public string thumbnailUrl { get; set; }
+    }
+
+}
